d78549b4c89b0748b237882ac72314804840c112
Alena139 <291407@gmail.com>
2016-07-07 15:29:59 +0300

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,53 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QCoreApplication>
+
+#include <QSettings>
+#include <QApplication>
+#include <QDebug>
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent),
+      ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    settingsFileName ="demosettings.ini";
+    //QApplication::applicationDirPath().left(1) + "demosettings.ini";
+    qDebug()<< QApplication::applicationDirPath().left(1)<<"\n\n"<<settingsFileName;
+
+    QSettings settings(settingsFileName, QSettings::NativeFormat);
+    QStringList keys=settings.allKeys();
+    qDebug()<<keys;
+
+    connect(ui->pb_getVal, SIGNAL(clicked()),this, SLOT (on_pb_getValue()));
+    connect(ui->pb_setVal, SIGNAL(clicked()),this, SLOT (on_pb_setValue()));
+}
+
+void MainWindow::on_pb_getValue()
+{
+    qDebug()<<"on_pb_getValue()";
+    QString key=ui->le_getKey->text();
+    QString val;
+
+    QSettings settings(settingsFileName, QSettings::NativeFormat);
+    val=settings.value(key,"default val").toString();
+    ui->le_getVal->setText(val);
+
+}
+
+void MainWindow::on_pb_setValue()
+{
+
+    qDebug()<<"on_pb_setValue()";
+    QString key=ui->le_setKey->text();
+    QString val=ui->le_setVal->text();
+
+    QSettings settings(settingsFileName, QSettings::NativeFormat);
+    settings.setValue(key, val);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
---
+++ mainwindow.h
@@ -0,0 +1,30 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+private:
+
+private slots:
+    void on_pb_getValue();
+    void on_pb_setValue();
+
+private:
+    QString settingsFileName;
+
+};
+
+#endif // MAINWINDOW_H
